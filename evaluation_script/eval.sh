e64133ca188749137a0d391d2cb7313630a04a98
rgw117 <rgw117@naver.com>
2021-12-19 12:31:59 +0000

    first commit

---
+++ evaluation_script/eval.sh
@@ -0,0 +1,4 @@
+python evaluation_script.py \
+	--annFile KAIST_annotation.json \
+	--rstFile ../src/jobs/2021-12-14_12h00m_/Epoch035_test_det.txt \
+	--evalFig KAIST_BENCHMARK.jpg
---
+++ src/train_eval.sh
@@ -0,0 +1,3 @@
+CUDA_VISIBLE_DEVICES=1,5 \
+OMP_NUM_THREADS=8 \
+python train_eval.py
